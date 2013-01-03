c0dab3c7629b2d0ac11e666efd41a73ae6539963
justin-hopper <justin.hopper@hp.com>
2013-01-03 11:21:55 -0800

    final changes before check-in

--- scripts/files/bootstrap_init-mysql.sh
+++ scripts/files/bootstrap_init-mysql.sh
@@ -3,7 +3,8 @@
 # Copies all the reddwarf code to the guest image
 sudo -u GUEST_USERNAME rsync -e'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' -avz --exclude='.*' HOST_USERNAME@10.0.0.1:PATH_REDDWARF/ /home/GUEST_USERNAME/reddwarf
 # Adds user to the sudoers file so they can do everything w/o a pass
-echo 'GUEST_USERNAME ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+echo 'GUEST_USERNAME ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/rdguest-sudoers
+sudo chmod 0440 /etc/sudoers.d/rdguest-sudoers
 # Do an apt-get update since its SUPER slow first time
 apt-get update
 # Add extras which is _only_ in pip.....
